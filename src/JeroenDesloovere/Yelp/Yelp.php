5b768cd6ef65d14d1f96f14bf10b26c66f5cb9d9
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-03-19 12:00:43 +0100

    PSR-2-code-styling applied

--- src/JeroenDesloovere/Yelp/Yelp.php
+++ src/JeroenDesloovere/Yelp/Yelp.php
@@ -10,143 +10,143 @@ namespace JeroenDesloovere\Yelp;
  */
 class Yelp
 {
-	// API url
-	const API_URL = 'http://api.yelp.com/';
+    // API url
+    const API_URL = 'http://api.yelp.com/';
 
-	// API version
-	const API_VERSION = 'v2';
+    // API version
+    const API_VERSION = 'v2';
 
-	// current version
-	const VERSION = '0.0.1';
+    // current version
+    const VERSION = '0.0.1';
 
-	/**
-	 * Consumer key
-	 *
-	 * @var string
-	 */
-	private $consumerKey;
+    /**
+     * Consumer key
+     *
+     * @var string
+     */
+    private $consumerKey;
 
-	/**
-	 * Consumer secret
-	 *
-	 * @var string
-	 */
-	private $consumerSecret;
+    /**
+     * Consumer secret
+     *
+     * @var string
+     */
+    private $consumerSecret;
 
-	/**
-	 * Token
-	 *
-	 * @var string
-	 */
-	private $token;
+    /**
+     * Token
+     *
+     * @var string
+     */
+    private $token;
 
-	/**
-	 * Token secret
-	 *
-	 * @var string
-	 */
-	private $tokenSecret;
+    /**
+     * Token secret
+     *
+     * @var string
+     */
+    private $tokenSecret;
 
-	/**
-	 * Construct
-	 *
-	 * @param string $consumerKey
-	 * @param string $consumerSecret
-	 * @param string $token
-	 * @param string $tokenSecret
-	 */
-	public function __construct($consumerKey, $consumerSecret, $token, $tokenSecret)
-	{
-		$this->consumerKey = (string) $consumerKey;
-		$this->consumerSecret = (string) $consumerSecret;
-		$this->token = (string) $token;
-		$this->tokenSecret = (string) $tokenSecret;
-	}
+    /**
+     * Construct
+     *
+     * @param string $consumerKey
+     * @param string $consumerSecret
+     * @param string $token
+     * @param string $tokenSecret
+     */
+    public function __construct($consumerKey, $consumerSecret, $token, $tokenSecret)
+    {
+        $this->consumerKey = (string) $consumerKey;
+        $this->consumerSecret = (string) $consumerSecret;
+        $this->token = (string) $token;
+        $this->tokenSecret = (string) $tokenSecret;
+    }
 
-	/**
-	 * Do call
-	 *
-	 * @param string $url The URL to call.
-	 * @param array[optional] $data The data to pass.
-	 */
-	private function doCall($url, $data = null)
-	{
-		// enter the path that the oauth library is in relation to the php file
-		require_once('oauth.php');
+    /**
+     * Do call
+     *
+     * @param string $url The URL to call.
+     * @param array[optional] $data The data to pass.
+     */
+    private function doCall($url, $data = null)
+    {
+        // enter the path that the oauth library is in relation to the php file
+        require_once('oauth.php');
 
-		// define unsigned url
-		$unsignedUrl = API_URL . API_VERSION . '/' . $url;
+        // define unsigned url
+        $unsignedUrl = API_URL . API_VERSION . '/' . $url;
 
-		// if data is set, add to url
-		if(!empty($data)) $unsignedUrl .= '?' . http_build_query($data);
+        // if data is set, add to url
+        if (!empty($data)) $unsignedUrl .= '?' . http_build_query($data);
 
-		// token object built using the OAuth library
-		$token = new OAuthToken($this->token, $this->tokenSecret);
+        // token object built using the OAuth library
+        $token = new OAuthToken($this->token, $this->tokenSecret);
 
-		// consumer object built using the OAuth library
-		$consumer = new OAuthConsumer($this->consumerKey, $this->consumerSecret);
+        // consumer object built using the OAuth library
+        $consumer = new OAuthConsumer($this->consumerKey, $this->consumerSecret);
 
-		// Yelp uses HMAC SHA1 encoding
-		$signatureMethod = new OAuthSignatureMethod_HMAC_SHA1();
+        // Yelp uses HMAC SHA1 encoding
+        $signatureMethod = new OAuthSignatureMethod_HMAC_SHA1();
 
-		// build OAuth Request using the OAuth PHP library. Uses the consumer and token object created above.
-		$oauthRequest = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $unsignedUrl);
+        // build OAuth Request using the OAuth PHP library. Uses the consumer and token object created above.
+        $oauthRequest = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $unsignedUrl);
 
-		// sign the request
-		$oauthRequest->sign_request($signatureMethod, $consumer, $token);
+        // sign the request
+        $oauthRequest->sign_request($signatureMethod, $consumer, $token);
 
-		// get the signed URL
-		$signedUrl = $oauthRequest->to_url();
+        // get the signed URL
+        $signedUrl = $oauthRequest->to_url();
 
-		// send Yelp API Call
-		$ch = curl_init($signedUrl);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
+        // send Yelp API Call
+        $ch = curl_init($signedUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
 
-		// Yelp response
-		$data = curl_exec($ch); 
+        // Yelp response
+        $data = curl_exec($ch); 
 
-		// close connection
-		curl_close($ch);
+        // close connection
+        curl_close($ch);
 
-		// handle Yelp response data
-		return(json_decode($data));
-	}
+        // handle Yelp response data
+        return(json_decode($data));
+    }
 
-	/**
-	 * Search for business
-	 *
-	 * @param string $value
-	 */
-	public function searchForBusiness($value)
-	{
-		// build url
-		$url = '/business/' . (string) $value;
+    /**
+     * Search for business
+     *
+     * @param string $value
+     */
+    public function searchForBusiness($value)
+    {
+        // build url
+        $url = '/business/' . (string) $value;
 
-		// do call
-		return $this->doCall($url);
-	}
+        // do call
+        return $this->doCall($url);
+    }
 
-	/**
-	 * Search for term
-	 *
-	 * @param string $term
-	 * @param string[optional] $location
-	 */
-	public function searchForTerm($term, $location = null)
-	{
-		// build url
-		$url = '/search';
+    /**
+     * Search for term
+     *
+     * @param string $term
+     * @param string[optional] $location
+     */
+    public function searchForTerm($term, $location = null)
+    {
+        // build url
+        $url = '/search';
 
-		// define data
-		$data = array('term' => $term);
+        // define data
+        $data = array('term' => $term);
 
-		// if location is defined
-		if($location !== null) $data['location'] = $location;
+        // if location is defined
+        if ($location !== null) $data['location'] = $location;
 
-		// do call
-		return $this->doCall($url, $data);
-	}
+        // do call
+        return $this->doCall($url, $data);
+    }
 }
 
 
@@ -155,6 +155,4 @@ class Yelp
  *
  * @author Jeroen Desloovere <jeroen@siesqo.be>
  */
-class YelpException extends Exception
-{	
-}
+class YelpException extends Exception {}
